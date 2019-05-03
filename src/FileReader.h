fb1f4cf701aa249f87c72fa51cd86e4c46f13d1d
guilhem hermet <hermetguilhem@gmail.com>
2019-05-03 17:25:41 +0200

    ajout fileRe&der.h

---
+++ src/FileReader.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include "Mesure.h"
+#include "Capteur.h"
+#include "Capteur.h"
+#include "Attribut.h"
+
+using namespace std;
+
+class FileReader
+{
+
+public:
+	FileReader();
+	~FileReader();
+	
+	void openCsvMesure(string chemin);
+	void openCsvCapteur(string chemin);
+	void openCsvAttribut(string chemin);
+
+	Mesure& LireligneMesure(Mesure&);
+	Capteur& LireligneCapteur(Capteur&);
+	Attribut& LireligneAttribut(Attribut&);
+private:
+
+	ifstream* csvMesure;
+	ifstream* csvCapteur;
+	ifstream* csvAttribut;
+};
+
