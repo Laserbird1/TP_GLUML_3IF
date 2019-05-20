f4716964c18662f085346260be14ab06ccf9c93d
Edouard de Brye <edebrye@insa-lyon.fr>
2019-05-20 17:05:06 +0200

    correction afficherVoisinPoint + ajout méthodes de calcul de distance entre deux points sur terre

--- src/Controleur.cpp
+++ src/Controleur.cpp
@@ -18,6 +18,7 @@ using namespace std;
 #include <set>
 #include <string>
 #include <Capteur.h>
+#include <cmath>
 
 //------------------------------------------------------ Include personnel
 
@@ -96,15 +97,56 @@ list<Capteur> Controleur::afficherVoisinsPoint(double longitude,double latitude,
 	{
 		double latTest = it->getLatitude();
 		double lngTest = it->getLongitude();
-		double x = longitude - latTest;
-		double y = latitude - lngTest;
+		double d = distance(latitude, longitude, latTest, lngTest);
 
-		if (x*x + y * y < r*r)
+		if (d < r)
 		{
 			res.push_front(*it);
 		}
 	}
 	return res;
+}
+
+long double distance(long double lat1, long double long1,
+	long double lat2, long double long2)
+{
+	// Convert the latitudes  
+	// and longitudes 
+	// from degree to radians. 
+	lat1 = toRadians(lat1);
+	long1 = toRadians(long1);
+	lat2 = toRadians(lat2);
+	long2 = toRadians(long2);
+
+	// Haversine Formula 
+	long double dlong = long2 - long1;
+	long double dlat = lat2 - lat1;
+
+	long double ans = pow(sin(dlat / 2), 2) +
+		cos(lat1) * cos(lat2) *
+		pow(sin(dlong / 2), 2);
+
+	ans = 2 * asin(sqrt(ans));
+
+	// Radius of Earth in  
+	// Kilometers, R = 6371 
+	// Use R = 3956 for miles 
+	long double R = 6371;
+
+	// Calculate the result 
+	ans = ans * R;
+
+	return ans;
+}
+
+long double toRadians(const long double degree)
+{
+	// cmath library in C++  
+	// defines the constant 
+	// M_PI as the value of 
+	// pi accurate to 1e-30 
+	long double one_deg = (M_PI) / 180;
+	return (one_deg * degree);
 }
 
 list<Capteur> Controleur::afficherAttributQualiteCapteur(string attributeID,double qualité,float s){}
